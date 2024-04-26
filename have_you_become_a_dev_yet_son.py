8f1676fb5ca1753e4c2e5a2ad42ac115597fc9ce
Ibrahim <17607802+cukcan@users.noreply.github.com>
2024-04-26 15:47:57 +0300

    Create have_you_become_a_dev_yet_son.py

---
+++ have_you_become_a_dev_yet_son.py
@@ -0,0 +1,9 @@
+while True:
+    answer = input("did you become a dev son? ")
+    if answer == "no":
+        print("You will work at this weekend")
+    elif answer == "yes":
+        print("I'm proud of you, now you deserve rest at weekend")
+        break
+    else:
+        print("answer with yes/no")
