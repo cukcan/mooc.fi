944eb8ae8d497456755c256d10bf2f433429fb27
Ibrahim <17607802+cukcan@users.noreply.github.com>
2024-05-05 22:26:20 +0300

    Create does_it_contain_vowels.py

---
+++ does_it_contain_vowels.py
@@ -0,0 +1,19 @@
+string = input("Please type in a string: ")
+a = "a"
+e = "e"
+o = "o"
+
+if a in string:
+    print("a found")
+else:
+    print("a not found")
+
+if e in string:
+    print("e found")
+else:
+    print("e not found")
+
+if o in string:
+    print("o found")
+else:
+    print("o not found")
