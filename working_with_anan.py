5e6c8a635a99c6f30171d6b7575aedf9b42c6c31
İBRAHİM BİTİKÇİOĞLU <17607802+cukcan@users.noreply.github.com>
2026-04-06 22:33:46 +0300

    Add number input and statistics calculation

---
+++ working_with_anan.py
@@ -0,0 +1,27 @@
+# Write your solution here
+print("Please type in integer numbers. Type in 0 to finish.")
+count = 0
+sum = 0
+positive = 0
+negative  = 0
+
+while True:
+    number = int(input("Number: "))
+        
+    if number == 0:
+        break
+    
+    count +=1
+    sum += number
+
+    if number > 0:
+        positive +=1
+    elif number < 0:
+        negative +=1
+
+mean = sum / count
+print(f"Numbers typed in {count}")   
+print(f"The sum of the numbers is {sum}") 
+print(f"The mean of the numbers is {mean}")
+print(f"Positive numbers {positive}")
+print(f"Negative numbers {negative}")
